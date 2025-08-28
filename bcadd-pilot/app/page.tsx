a3975111908904d85d52b70a3d52107c1018583c
Josh <dw22682@bristol.ac.uk>
2025-08-28 15:32:12 +0100

    base app

---
+++ bcadd-pilot/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-[#131313]">
+      <main>
+        <h1 className="text-center text-white">Bcadd Pilot</h1>
+      </main>
+    </div>
+  );
+}
