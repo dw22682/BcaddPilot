400dc41aaa913b40cf0607bde871d32cc1e01abe
Josh <dw22682@bristol.ac.uk>
2025-09-01 17:39:54 +0100

    this shouldnt fix it

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className="items-center bg-[#131313]">
         <h1 className="text-center text-white">Bcadd Pilot</h1>
         <div className="grid grid-cols-2 items-center justify-items-center px-5 py-5">
-          <Link href="/BcaddPilot/bcaddPilotTest-0.1.1.exe" download="bcadd-no-storage.exe">
+          <Link href="/bcaddPilotTest-0.1.1.exe" download="bcadd-no-storage.exe">
             <button className="py-3 px-5 text-amber-50 border-white border hover:text-blue-400">
               Download No Storage
             </button>
